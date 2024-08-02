8fff9a3f36636d437663851be685087353686e58
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-02 11:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const url = location.href;
+  const urlLength = url.length;
+  alert(`The length of the URL is: ${urlLength}`);
+});
